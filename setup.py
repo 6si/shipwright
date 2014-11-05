f5b69965ee4f0953a1e2a85fd8138a5850565d53
Scott Robertson <scott@6si.com>
2014-11-05 15:36:51 -0800

    require docker-py >= 0.5.3

--- setup.py
+++ setup.py
@@ -32,15 +32,10 @@ setup(
     install_requires=[
         'docopt',
         'zipper',
-        'docker-py>=0.4.1-dev',
+        'requests>=2.4.0',
+        'docker-py==0.5.3',
         'GitPython==0.3.2.RC1',
 
-    ],
-    dependency_links = [
-        # we need the version of docker-py that respectst the exclude directive
-        # switch this to the appropriate docker-py version when something greater than
-        # 0.4.1 is released.
-        'git+https://github.com/docker/docker-py.git@c76fd8d91481d33cee6fcfaa61025c12caa95b98#egg=docker-py-0.4.1-dev'
     ],
     author_email='scott@6sense.com',
     description='Multi docker image build management',
