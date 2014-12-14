0f199fa428a73d4398e22b761bae6b137167b92f
Scott Robertson <scott@6si.com>
2014-12-14 10:43:25 -0800

    checkpoint

--- shipwright/dependencies.py
+++ shipwright/dependencies.py
@@ -1,17 +1,72 @@
 from __future__ import absolute_import
 
-from collections import  defaultdict
+from collections import  defaultdict, namedtuple
 
 import zipper
+
+from .fn import curry
+
+
+
+def eval(specifiers, targets):
+  transforms = []
+
+  for spec in specifiers:
+    if spec.startswith('='):
+      # '=blah' -> exact('blah')
+      transforms.append(exact(spec[1:]))
+    elif spec.startswith('^'):
+      # '^blah's -> descendants('blah')
+      transforms.append(descendants(spec[1:]))
+    elif spec.startswith('-'):
+      # '-blah's -> exclude('blah')
+      transforms.append(exclude(spec[1:]))
+    else:
+      transforms.append(upto(spec))
+
+  tree = make_tree(targets)
+
+  for trans in transforms:
+    tree = trans(tree)
+
+  return tree
+
+
+
+def union(inclusions, tree):
+  return make_tree(reduce(lambda p,f: p | set(f(tree)), inclusions, set()))
+
+def eval2(specifiers, targets):
+  inclusions = []
+  exclusions = []
+
+  for spec in specifiers:
+    if spec.startswith('='):
+      # '=blah' -> exact('blah')
+      inclusions.append(exact(spec[1:]))
+    elif spec.startswith('^'):
+      # '^blah's -> descendants('blah')
+      inclusions.append(descendants(spec[1:]))
+    elif spec.startswith('-'):
+      # '-blah's -> exclude('blah')
+      exclusions.append(exclude(spec[1:]))
+    else:
+      inclusions.append(upto(spec))
+
+  tree = union(inclusions, make_tree(targets))
+
+  return fn.compose(exclusions)(tree)
+
+
 
 # Ref = git.Ref
 # item = <anything>
 # [Ref] -> Ref -> [Containers] -> [item]
-def needs_building(targets):
+def needs_building(tree):
   """
   """
   
-  gen = breadth_first_iter(make_tree(targets))
+  gen = breadth_first_iter(tree)
   root = next(gen) # skip root
   loc = next(gen)
 
@@ -44,7 +99,7 @@ def needs_building(targets):
   return skip, needs
 
 
-
+Node = namedtuple('Node', 'value, children')
 
 # [Container] -> Loc Container
 
@@ -53,35 +108,31 @@ def make_tree(containers):
   Converts a list of containers into a tree represented by a zipper.
   see http://en.wikipedia.org/wiki/Zipper_(data_structure)
 
-  >>> containers = [
-  ...   Container('shipwright_test/2', 'path1/Dockerfile', 'shipwright_test/1'),
-  ...   Container('shipwright_test/1', 'path1/Dockerfile', 'ubuntu'),
-  ...   Container('shipwright_test/3', 'path1/Dockerfile', 'shipwright_test/2'),
-  ...   Container('shipwright_test/independent', 'path1/Dockerfile', 'ubuntu'),
-  ... ]
+  >>> from .dependencies import targets
 
-  >>> root = make_tree(containers)
+  >>> root = make_tree(targets)
 
   >>> root  # doctest: +ELLIPSIS
   <zipper.Loc(None) ...>
 
-  >>> root.children()  # doctest: +ELLIPSIS
-  [Container(name='shipwright_test/1', ...), Container(name='shipwright_test/independent', ...)]
+  >>> _names(root)
+  ['shipwright_test/1', 'shipwright_test/independent', 'shipwright_test/2', 'shipwright_test/3']
 
   >>> root.down().node()  # doctest: +ELLIPSIS
-  Container(name='shipwright_test/1', ...)
+  Target(container=Container(name='shipwright_test/1', ...)
 
-  >>> root.down().children()  # doctest: +ELLIPSIS
-  [Container(name='shipwright_test/2', ...)]
+  >>> _names(root.down())  # doctest: +ELLIPSIS
+  ['shipwright_test/2', 'shipwright_test/3']
+  
 
   >>> root.down().down().node()  # doctest: +ELLIPSIS
-  Container(name='shipwright_test/2', ...)
+  Target(container=Container(name='shipwright_test/2', ...)
 
-  >>> root.down().down().children()  # doctest: +ELLIPSIS
-  [Container(name='shipwright_test/3', ...)]
+  >>> _names(root.down().down())  # doctest: +ELLIPSIS
+  ['shipwright_test/3']
   
   >>> root.down().right().node() # doctest: +ELLIPSIS
-  Container(name='shipwright_test/independent', ...)
+  Target(container=Container(name='shipwright_test/independent', ...)
 
   """
 
@@ -94,7 +145,7 @@ def make_tree(containers):
 
   # sort children by name, to make testing eaiser
   for node, children in graph.items():
-    graph[node] = sorted(children)
+    graph[node] = tuple(sorted(children))
 
   def children(item):
     return graph[item]
@@ -115,16 +166,11 @@ def breadth_first_iter(loc):
   Given a loctation node (from a zipper) walk it's children in breadth first
   order.
 
-  >>> containers = [
-  ...   Container('shipwright_test/2', 'path1/Dockerfile', 'shipwright_test/1'),
-  ...   Container('shipwright_test/1', 'path1/Dockerfile', 'ubuntu'),
-  ...   Container('shipwright_test/3', 'path1/Dockerfile', 'shipwright_test/2'),
-  ...   Container('shipwright_test/independent', 'path1/Dockerfile', 'ubuntu'),
-  ... ]
+  >>> from .dependencies import targets
 
-  >>> tree = make_tree(containers)
+  >>> tree = make_tree(targets)
   >>> [loc.node() for loc in breadth_first_iter(tree)] # doctest: +ELLIPSIS
-  [None, Container(name='shipwright_test/1', ...), Container(name='shipwright_test/independent', ...), Container(name='shipwright_test/2', ...), Container(name='shipwright_test/3', ...)]
+  [None, ...Container(name='shipwright_test/1', ...), ...Container(name='shipwright_test/independent', ...), ...Container(name='shipwright_test/2', ...), ...Container(name='shipwright_test/3', ...)]
 
   """
 
@@ -139,4 +185,133 @@ def breadth_first_iter(loc):
       tocheck.append(child)
       child = child.right()
 
+
+#
+@curry
+def is_target(name, target):
+  """
+  >>> from . import Target
+  >>> from . import Container
+
+  >>> target = Target(Container('test', None, None, None), None, None, None)
+  >>> is_target('test', target)
+  True
+  """
+  return target.container.name == name
+
+@curry
+def fmap(func, loc):
+  return func(loc.node())
+
+def lineage(loc):
+  results = []
+  while loc.path:
+    node = loc.node()
+    results.append(node)
+    loc = loc.up()
+  return results
+
+def brood(loc):
+  return [loc.node() for loc in breadth_first_iter(loc)][1:]
+
+#all = make_tree
+
+
+@curry
+def upto(target, tree):
+  """
+  returns target and everything it depends on
+
+  >>> from .dependencies import targets
+  >>> tree = upto('shipwright_test/2', make_tree(targets))
+  >>> _names(tree)
+  ['shipwright_test/1', 'shipwright_test/2']
+  """
+
+  loc = tree.find(fmap(is_target(target)))
+ 
+  return make_tree(lineage(loc))
+
+@curry
+def descendants(target, tree):
+  """
+  Returns a target it's dependencies and 
+  everything that depends on it
+
+  >>> from .dependencies import targets
+  >>> tree = descendants('shipwright_test/2', make_tree(targets))
+  >>> _names(tree)
+  ['shipwright_test/1', 'shipwright_test/2', 'shipwright_test/3']
+  """
+
+  loc = tree.find(fmap(is_target(target)))
+  return make_tree(lineage(loc) + brood(loc))
+
+@curry
+def exact(target, tree):
+  """
+  Returns only the target.
+
+  >>> from .dependencies import targets
+  >>> tree = exact('shipwright_test/2', make_tree(targets))
+  >>> _names(tree)
+  ['shipwright_test/2']
   
+  """
+
+  loc = tree.find(fmap(is_target(target)))
+
+  return make_tree([loc.node()])
+
+
+def exclude(target, tree):
+  """
+  Returns everything but the target and it's descendants.
+
+  >>> from .dependencies import targets
+  >>> tree = exclude('shipwright_test/2', make_tree(targets))
+  >>> _names(tree) # doctest: +ELLIPSIS
+  ['shipwright_test/1', 'shipwright_test/independent']
+
+  """
+  loc = tree.find(fmap(is_target(target)))
+ 
+  ret = loc.remove()
+  return ret.top()
+
+
+### Test methods ###
+def _names(tree):
+ return [n.name for n in brood(tree)]
+
+def setup_module(module):
+  from .container import Container
+  from . import Target
+
+  module.targets = [
+    Target(
+      Container('shipwright_test/2', 'path2/', 'path2/Dockerfile', 'shipwright_test/1'),
+      'abc',
+      3,
+      3
+    ),
+    Target(
+      Container('shipwright_test/1', 'path1/', 'path1/Dockerfile', 'ubuntu'),
+      'abc',
+      3,
+      3
+    ),
+    Target(
+      Container('shipwright_test/3', 'path3/', 'path3/Dockerfile', 'shipwright_test/2'),
+      'abc',
+      3,
+      3
+    ),
+    Target(
+      Container('shipwright_test/independent', 'independent', 'path1/Dockerfile', 'ubuntu'),
+      'abc',
+      3,
+      3
+    )
+  ]
+
