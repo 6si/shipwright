8c9a6be52520fa9dee34faa3ee9063bff3582543
Thomas Grainger <tom.grainger@procensus.com>
2016-06-15 12:07:01 +0100

    unify python2 and python3 json_loads implementation

--- shipwright/compat.py
+++ shipwright/compat.py
@@ -1,15 +1,8 @@
 from __future__ import absolute_import
 
 import json
-import sys
 
-PY2 = sys.version_info[0] == 2
-
-
-if PY2:
-    json_loads = json.loads
-else:
-    def json_loads(s, **kwargs):
-        if isinstance(s, bytes):
-            s = s.decode('utf8')
-        return json.loads(s, **kwargs)
+def json_loads(s, **kwargs):
+    if isinstance(s, bytes):
+        s = s.decode('utf8')
+    return json.loads(s, **kwargs)
