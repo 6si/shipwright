2df4cfa23b888237d0f209ddf92551598818ca5a
Thomas Grainger <tom.grainger@procensus.com>
2016-08-24 11:00:29 +0100

    push images in parallel

--- shipwright/push.py
+++ shipwright/push.py
@@ -1,12 +1,14 @@
 from __future__ import absolute_import
 
+import itertools
+
 from . import compat
 
 
 def do_push(client, images):
-    for image in images:
-        for evt in push(client, image):
-            yield evt
+    push_results = [push(client, image) for image in images]
+    for evt in itertools.chain.from_iterable(push_results):
+        yield evt
 
 
 def push(client, image_tag):
