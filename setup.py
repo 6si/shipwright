5f973ad55bd9c045d8b4477cc78cc3f1439c5bf6
Thomas Grainger <tom.grainger@procensus.com>
2017-01-11 18:34:08 +0000

    add python 3.6

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Development Status :: 4 - Beta',
         'Natural Language :: English',
         'Intended Audience :: Developers',
